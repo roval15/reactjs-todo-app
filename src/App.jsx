9156e4bdb2de1f10607918d8ca1c1fd0b33e1fe7
Ronnel Valencia <valencia.ronnel015@gmail.com>
2023-08-26 11:14:08 +0800

    feat: added add task form with styling
    1. Created the label
    2. Created the input box
    3. Created the "Add task" button
    4. Added a styling for all of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    "Hi"
-  )
+    <form className="new-todo-form">
+      <div className="form-row">
+        <label htmlFor="new-todo-input">What needs to be done?</label>
+        <input type="text" id="new-todo-input" />
+      </div>
+      <button className="button-80">Add Task</button>
+    </form>
+  );
 }
 
-export default App
+export default App;
