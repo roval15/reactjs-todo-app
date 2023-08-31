619570b59561cbfb463c858f2d0025eaf1299bfa
roval15 <valencia.ronnel015@gmail.com>
2023-08-31 21:26:45 +0800

    feat: included the to do list
    + check box
    + to do label
    + delete task button
    + added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,37 @@ import "./App.css";
 
 function App() {
   return (
-    <form className="new-todo-form">
-      <div className="form-row">
-        <label htmlFor="new-todo-input">What needs to be done?</label>
-        <input type="text" id="new-todo-input" />
-      </div>
-      <button className="button-80">Add Task</button>
-    </form>
+    <>
+      <form className="new-todo-form">
+        <div className="form-row">
+          <label htmlFor="new-todo-input">What needs to be done?</label>
+          <input
+            type="text"
+            id="new-todo-input"
+            placeholder="Enter your task here"
+          />
+        </div>
+        <button className="button-80">Add Task</button>
+      </form>
+      <h1 className="new-todo-title">To Do List:</h1>
+      <ul className="new-todo-list">
+        <li>
+          <label className="new-todo-container">
+            <input type="checkbox"/> 
+            Item 1
+            <button className="btn delete-todo-task">Delete</button>
+          </label>
+        </li>
+        <li>
+        <label className="new-todo-container">
+            <input type="checkbox"/> 
+            Item 1
+            <button className="btn delete-todo-task">Delete</button>
+          </label>
+        </li>
+      </ul>
+
+    </>
   );
 }
 
