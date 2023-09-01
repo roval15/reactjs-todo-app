25ced7869c1a0790280c3c036a6137bcb6a18ef2
roval15 <valencia.ronnel015@gmail.com>
2023-09-01 20:54:03 +0800

    feat: tasks are automatically added to the list
    + checkbox toggle for to dos
    + tasks are added on the list

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,68 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  console.log(todos);
+
   return (
     <>
-      <form className="new-todo-form">
+      <form onSubmit={handleSubmit} className="new-todo-form">
         <div className="form-row">
           <label htmlFor="new-todo-input">What needs to be done?</label>
           <input
+            value={newItem}
             type="text"
             id="new-todo-input"
-            placeholder="Enter your task here"
+            onChange={(e) => setNewItem(e.target.value)}
           />
         </div>
         <button className="button-80">Add Task</button>
       </form>
       <h1 className="new-todo-title">To Do List:</h1>
       <ul className="new-todo-list">
-        <li>
-          <label className="new-todo-container">
-            <input type="checkbox"/> 
-            Item 1
-            <button className="btn delete-todo-task">Delete</button>
-          </label>
-        </li>
-        <li>
-        <label className="new-todo-container">
-            <input type="checkbox"/> 
-            Item 1
-            <button className="btn delete-todo-task">Delete</button>
-          </label>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label className="new-todo-container">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+                <button className="btn delete-todo-task">Delete</button>
+              </label>
+            </li>
+          );
+        })}
       </ul>
-
     </>
   );
 }
